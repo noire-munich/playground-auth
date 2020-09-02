7ba677ed775e306ef06eab0ccec5067e35f231f0
Thomas Seillan <smokinglaslo@gmail.com>
2020-09-02 12:13:48 +0200

    Added form sign up for UserTools.

--- web/src/components/UserTools/UserTools.js
+++ web/src/components/UserTools/UserTools.js
@@ -1,15 +1,37 @@
+import { useCallback, useReducer, useState } from 'react'
 import { useAuth } from '@redwoodjs/auth'
 
 const Auth0 = () => {
+  const [data, record] = useReducer(
+    (state, action) => ({ ...state, ...action }),
+    {}
+  )
+  const [error, setError] = useState()
+  const [message, setMessage] = useState()
+
   const {
     loading,
     logIn,
     logOut,
+    signUp,
     isAuthenticated,
     userMetadata,
     currentUser,
     type,
   } = useAuth()
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault()
+      // Put whatever logic one needs here to check the data.
+      // logic...
+      // Then do the signUp.
+      signUp(data)
+        .then(setMessage)
+        .catch(({ message }) => setError(message))
+    },
+    [signUp, data]
+  )
 
   if (loading) {
     return 'Loading...'
@@ -38,6 +60,28 @@ const Auth0 = () => {
         <br />
         {JSON.stringify(currentUser, 2)}
       </code>
+      <br />
+      <form onSubmit={onSubmit} action={'#'}>
+        {error && <p>Error: {error}</p>}
+        {message && <p>Message: {message}</p>}
+        <label htmlFor={`${type}__email`}>Email</label>
+        <input
+          id={`${type}__email`}
+          name={'email'}
+          onChange={({ currentTarget: { value } }) => record({ email: value })}
+        />
+        <label htmlFor={`${type}__password`}>Password</label>
+        <input
+          type={'password'}
+          id={`${type}__password`}
+          name={'password'}
+          placeholder={'password'}
+          onChange={({ currentTarget: { value } }) =>
+            record({ password: value })
+          }
+        />
+        <button type={'submit'}>Sign Up</button>
+      </form>
     </>
   )
 }
