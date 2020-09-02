29504b8fc9ec7cfc02867accb13ce2569353ec8b
Thomas Seillan <smokinglaslo@gmail.com>
2020-09-02 12:14:03 +0200

    Some lint.

--- api/src/functions/graphql.js
+++ api/src/functions/graphql.js
@@ -6,7 +6,6 @@ import {
 import schemas from 'src/graphql/**/*.{js,ts}'
 import services from 'src/services/**/*.{js,ts}'
 
-
 import { getCurrentUser } from 'src/lib/auth.js'
 
 export const handler = createGraphQLHandler({
